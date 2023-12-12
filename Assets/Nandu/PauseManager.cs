e5afa2ba5e2e816d92ce54cb6e2a3f98543f52e7
Srinanda Yallapragada <srinanda.yallapragada@gmail.com>
2023-12-12 17:35:35 -0500

    pasuse menu functionality

---
+++ Assets/Nandu/PauseManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        //pause state of game at some point
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+        //resume game state
+    }
+    public void GoToMainMenu()
+    {
+        Debug.Log("Switching to main menu");
+        //add scene manager here to load the correct scene
+    }
+}
