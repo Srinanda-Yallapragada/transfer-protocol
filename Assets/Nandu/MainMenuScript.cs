d0fc715ca409c4cc14764c55a5c3462766f1bf4e
Srinanda Yallapragada <srinanda.yallapragada@gmail.com>
2023-12-12 16:54:24 -0500

    added main menu scene

---
+++ Assets/Nandu/MainMenuScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        Debug.Log("scenemanager to main game");
+    }
+    public void QuitGame()
+    {
+        Debug.Log(" quit game exit");
+    }
+}
